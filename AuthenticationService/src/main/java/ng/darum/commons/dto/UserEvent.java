0c2c14b14341ff4ac320edc08023378e499b3c97
Imanuelkarl <appdeveloperemmanuel@gmail.com>
2025-10-30 11:24:13 +0100

    Improved security

--- AuthenticationService/src/main/java/ng/darum/commons/dto/UserEvent.java
+++ AuthenticationService/src/main/java/ng/darum/commons/dto/UserEvent.java
@@ -1,16 +1,15 @@
 package ng.darum.commons.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ng.darum.employee.enums.Role;
+import ng.darum.auth.enums.Role;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserCreatedEvent {
+public class UserEvent {
+    private Long id;
     private String email;
     private Role role;
     private String password;
--- EmployeeManagementService/src/main/java/ng/darum/commons/dto/UserEvent.java
+++ EmployeeManagementService/src/main/java/ng/darum/commons/dto/UserEvent.java
@@ -1,14 +1,17 @@
 package ng.darum.commons.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ng.darum.auth.enums.Role;
+import ng.darum.employee.enums.Role;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserCreatedEvent {
+public class UserEvent {
+    private Long id;
     private String email;
     private Role role;
     private String password;
